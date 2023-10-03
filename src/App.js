b8c254a754abc27753c18c432de9ddb59fe894fe
Amare <141082323+amareb@users.noreply.github.com>
2023-10-04 01:17:37 +1000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Checkout from "./Checkout";
+import Login from "./Login";
+import React, { useEffect } from "react";
+import { useStateValue } from "./Stateprovider";
+import { auth } from "./Firebase"; // Remove the space before "./Firebase";
+
+function App() {
+  const [{ user }, dispatch] = useStateValue();
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+
+    // Clean up the listener when the component unmounts
+    return () => {
+      unsubscribe();
+    };
+  }, [dispatch]); // Include dispatch as a dependency
+
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <Checkout />
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
